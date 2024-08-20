4c9b6701f71ddad640fd9df95aca8a764a44facb
Miguel Ceballos <mceballosleonides@gmail.com>
2024-08-19 22:53:08 -0600

    fix(styles): soluciona el desplazamiento de la categoría de tarea.

--- src/modules/tasks/views/TaskView.vue
+++ src/modules/tasks/views/TaskView.vue
@@ -15,7 +15,7 @@
             <div class="mt-1">
               <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm" />
             </div>
-            <div class="overflow-hidden">
+            <div class="w-full overflow-hidden">
               <div class="flex flex-col">
                 <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
                 <p class="truncate text-xs">
@@ -35,162 +35,17 @@
             <div class="mt-1">
               <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm" />
             </div>
-            <div class="overflow-hidden">
+            <div class="w-full overflow-hidden">
               <div class="flex flex-col">
                 <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                <p class="truncate text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                  aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                  voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                </p>
+                <p class="text-xs hidden"></p>
               </div>
-              <div class="flex justify-end">
+              <div class="flex w-full justify-end">
                 <button class="btn btn-ghost btn-xs justify-end">#Home</button>
               </div>
             </div>
           </div>
         </li>
-        <li class="border-b border-gray-700">
-          <div class="flex w-full space-x-2">
-            <div class="mt-1">
-              <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm" />
-            </div>
-            <div class="overflow-hidden">
-              <div class="flex flex-col">
-                <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                <p class="truncate text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                  aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                  voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                </p>
-              </div>
-              <div class="flex justify-end">
-                <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="border-b border-gray-700">
-          <div class="flex w-full space-x-2">
-            <div class="mt-1">
-              <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm" />
-            </div>
-            <div class="overflow-hidden">
-              <div class="flex flex-col">
-                <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                <p class="truncate text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                  aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                  voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                </p>
-              </div>
-              <div class="flex justify-end">
-                <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="border-b border-gray-700">
-          <div class="flex w-full space-x-2">
-            <div class="mt-1">
-              <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm" />
-            </div>
-            <div class="overflow-hidden">
-              <div class="flex flex-col">
-                <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                <p class="truncate text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                  aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                  voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                </p>
-              </div>
-              <div class="flex justify-end">
-                <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="border-b border-gray-700">
-          <div class="flex w-full space-x-2">
-            <div class="mt-1">
-              <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm" />
-            </div>
-            <div class="overflow-hidden">
-              <div class="flex flex-col">
-                <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                <p class="truncate text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                  aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                  voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                </p>
-              </div>
-              <div class="flex justify-end">
-                <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="border-b border-gray-700">
-          <div class="flex w-full space-x-2">
-            <div class="mt-1">
-              <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm" />
-            </div>
-            <div class="overflow-hidden">
-              <div class="flex flex-col">
-                <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                <p class="truncate text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                  aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                  voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                </p>
-              </div>
-              <div class="flex justify-end">
-                <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="border-b border-gray-700">
-          <div class="flex w-full space-x-2">
-            <div class="mt-1">
-              <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm" />
-            </div>
-            <div class="overflow-hidden">
-              <div class="flex flex-col">
-                <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                <p class="truncate text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                  aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                  voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                </p>
-              </div>
-              <div class="flex justify-end">
-                <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="border-b border-gray-700">
-          <div class="flex w-full space-x-2">
-            <div class="mt-1">
-              <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm" />
-            </div>
-            <div class="overflow-hidden">
-              <div class="flex flex-col">
-                <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetssdur.</h3>
-                <p class="truncate text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                  aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                  voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                </p>
-              </div>
-              <div class="flex justify-end">
-                <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-              </div>
-            </div>
-          </div>
-        </li>
-
       </ul>
     </div>
   </div>
