a04eb38a65d57a083121531fc114f8f9fb78f03f
Miguel Ceballos <mceballosleonides@gmail.com>
2024-08-25 14:37:06 -0600

    fix(icons): quita la prop de size porque no se mostraban los iconos al iniciar la app. Aplica formato de Prettier

--- src/modules/common/icons/PlusIcon.vue
+++ src/modules/common/icons/PlusIcon.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
-interface Props {
-  size: string
-}
-defineProps<Props>()
-</script>
-
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-       :class="`btn btn-xs size-${size} btn-circle btn-outline btn-accent group-hover:btn-active`"
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="btn btn-xs size-6 btn-circle btn-outline btn-accent group-hover:btn-active"
   >
     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
   </svg>
