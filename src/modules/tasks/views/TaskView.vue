5843fc8ae28ff12c401fc59fa17aa1f9414300d1
Miguel Ceballos <mceballosleonides@gmail.com>
2024-08-25 17:20:24 -0600

    feat(tasks): muestra en una lista las tareas del ususario.

--- src/modules/tasks/views/TaskView.vue
+++ src/modules/tasks/views/TaskView.vue
@@ -1,186 +1,44 @@
 <script setup lang="ts">
+import CheckCircleIcon from '@/modules/common/icons/CheckCircleIcon.vue';
+import TaskForm from '@/modules/tasks/components/TaskForm.vue';
+import { useTasksStore } from '@/modules/tasks/store/tasks.store';
 
-import CheckCircleIcon from '@/modules/common/icons/CheckCircleIcon.vue'
-import TaskForm from '@/modules/tasks/components/TaskForm.vue'
+const tasksStore = useTasksStore();
 
+// console.log(tasksStore.tasks)
 </script>
 
 <template>
-  <div class="bg-[#171C28]  w-full h-screen">
+  <div class="bg-[#171C28] w-full min-h-full">
     <div class="max-w-4xl overflow-x-hidden px-10 lg:mx-auto my-6 sm:my-16">
       <div class="space-y-6">
         <div class="flex space-x-4 justify-between items-end">
           <h2 class="text-2xl text-gray-300 md:text-3xl font-bold">Tasks</h2>
-          <p class="text-sm pr-4 flex justify-center items-center gap-1">
-            <check-circle-icon size="5" />
-            4 Tasks
-          </p>
+          <div class="flex gap-2">
+            <check-circle-icon size="6" />
+            <p class="text-md">{{ tasksStore.tasks.length }} Tasks</p>
+          </div>
         </div>
         <div>
           <task-form />
         </div>
-        <ul>
+        <ul v-if="tasksStore.tasks">
           <li
-            class="border-b hover:scale-[1.003] border-gray-800 hover:cursor-pointer hover:dark:bg-[#1E2330] px-1 py-2 rounded-t-md">
+            v-for="task in tasksStore.tasks"
+            :key="task.id"
+            class="border-b hover:scale-[1.003] border-gray-800 hover:cursor-pointer hover:dark:bg-[#1E2330] px-1 py-2 rounded-t-md group transition-duration-200"
+          >
             <div class="flex w-full space-x-2">
               <div class="mt-1">
-                <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm hover:border-2" />
+                <input
+                  type="checkbox"
+                  class="checkbox checkbox-primary rounded-full checkbox-sm hover:border-2"
+                />
               </div>
               <div class="w-full overflow-hidden">
                 <div class="flex flex-col">
-                  <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                  <p class="truncate text-xs">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                    aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                    voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                  </p>
-                </div>
-                <div class="flex justify-end">
-                  <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li
-            class="border-b hover:scale-[1.003] border-gray-800 hover:cursor-pointer hover:dark:bg-[#1E2330] px-1 py-2 rounded-t-md">
-            <div class="flex w-full space-x-2">
-              <div class="mt-1">
-                <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm hover:border-2" />
-              </div>
-              <div class="w-full overflow-hidden">
-                <div class="flex flex-col">
-                  <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                  <p class="truncate text-xs">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                    aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                    voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                  </p>
-                </div>
-                <div class="flex justify-end">
-                  <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li
-            class="border-b hover:scale-[1.003] border-gray-800 hover:cursor-pointer hover:dark:bg-[#1E2330] px-1 py-2 rounded-t-md">
-            <div class="flex w-full space-x-2">
-              <div class="mt-1">
-                <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm hover:border-2" />
-              </div>
-              <div class="w-full overflow-hidden">
-                <div class="flex flex-col">
-                  <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                  <p class="truncate text-xs">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                    aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                    voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                  </p>
-                </div>
-                <div class="flex justify-end">
-                  <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li
-            class="border-b hover:scale-[1.003] border-gray-800 hover:cursor-pointer hover:dark:bg-[#1E2330] px-1 py-2 rounded-t-md">
-            <div class="flex w-full space-x-2">
-              <div class="mt-1">
-                <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm hover:border-2" />
-              </div>
-              <div class="w-full overflow-hidden">
-                <div class="flex flex-col">
-                  <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                  <p class="truncate text-xs">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                    aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                    voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                  </p>
-                </div>
-                <div class="flex justify-end">
-                  <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li
-            class="border-b hover:scale-[1.003] border-gray-800 hover:cursor-pointer hover:dark:bg-[#1E2330] px-1 py-2 rounded-t-md">
-            <div class="flex w-full space-x-2">
-              <div class="mt-1">
-                <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm hover:border-2" />
-              </div>
-              <div class="w-full overflow-hidden">
-                <div class="flex flex-col">
-                  <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                  <p class="truncate text-xs">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                    aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                    voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                  </p>
-                </div>
-                <div class="flex justify-end">
-                  <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li
-            class="border-b hover:scale-[1.003] border-gray-800 hover:cursor-pointer hover:dark:bg-[#1E2330] px-1 py-2 rounded-t-md">
-            <div class="flex w-full space-x-2">
-              <div class="mt-1">
-                <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm hover:border-2" />
-              </div>
-              <div class="w-full overflow-hidden">
-                <div class="flex flex-col">
-                  <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                  <p class="truncate text-xs">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                    aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                    voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                  </p>
-                </div>
-                <div class="flex justify-end">
-                  <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li
-            class="border-b hover:scale-[1.003] border-gray-800 hover:cursor-pointer hover:dark:bg-[#1E2330] px-1 py-2 rounded-t-md">
-            <div class="flex w-full space-x-2">
-              <div class="mt-1">
-                <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm hover:border-2" />
-              </div>
-              <div class="w-full overflow-hidden">
-                <div class="flex flex-col">
-                  <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                  <p class="truncate text-xs">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                    aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                    voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                  </p>
-                </div>
-                <div class="flex justify-end">
-                  <button class="btn btn-ghost btn-xs justify-end">#Home</button>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li
-            class="border-b hover:scale-[1.003] border-gray-800 hover:cursor-pointer hover:dark:bg-[#1E2330] px-1 py-2 rounded-t-md">
-            <div class="flex w-full space-x-2">
-              <div class="mt-1">
-                <input type="checkbox" class="checkbox checkbox-primary rounded-full checkbox-sm hover:border-2" />
-              </div>
-              <div class="w-full overflow-hidden">
-                <div class="flex flex-col">
-                  <h3 class="text-gray-200 text-sm mb-1">Lorem ipsum dolor sit amet, consectetur.</h3>
-                  <p class="truncate text-xs">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-                    aspernatur consequuntur distinctio dolorum eius fugit id illum modi nostrum quisquam tempora,
-                    voluptatibus. Ad cum cumque enim obcaecati odio quisquam voluptate.
-                  </p>
+                  <h3 class="text-gray-200 text-sm mb-1 group-hover:text-accent group-hover:font-semibold">{{ task.title }}</h3>
+                  <p class="truncate text-xs">{{ task.description }}</p>
                 </div>
                 <div class="flex justify-end">
                   <button class="btn btn-ghost btn-xs justify-end">#Home</button>
@@ -189,11 +47,14 @@ import TaskForm from '@/modules/tasks/components/TaskForm.vue'
             </div>
           </li>
         </ul>
+        <ul v-else>
+          <li>
+            <p>No hay tareas existentes.</p>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
