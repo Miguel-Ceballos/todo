82bfee4f998273c9ffe09b02c6674099594279d9
Miguel Ceballos <mceballosleonides@gmail.com>
2024-09-04 18:09:33 -0600

    Crea componente Dropwdown

---
+++ src/modules/categories/components/CategoryDropDownComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+import EllipsisVerticalIcon from '@/modules/common/icons/EllipsisVerticalIcon.vue';
+import { ref } from 'vue';
+
+const openDropdown = ref(false);
+
+const toggleDropdown = () => {
+  openDropdown.value = !openDropdown.value;
+};
+</script>
+
+<template>
+  <div class="dropdown">
+    <div
+      @click="toggleDropdown"
+      tabindex="0"
+      class="p-1 bg-gray-800 rounded-full text-xs text-white hover:bg-gray-700 hover:cursor-pointer"
+    >
+      <ellipsis-vertical-icon />
+    </div>
+    <ul
+      @close="toggleDropdown"
+      v-if="openDropdown"
+      class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+    >
+      <li><a>Edit</a></li>
+      <li><a>Delete</a></li>
+    </ul>
+  </div>
+</template>
