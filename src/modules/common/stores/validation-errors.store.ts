e92e2f4ff6ddb500bf0b7752fb3d32c8bd0df169
Miguel Ceballos <mceballosleonides@gmail.com>
2024-09-06 15:42:27 -0600

    feat(errors-store): crea errors store para reutilizar la función que valida los errores de los formularios.

---
+++ src/modules/common/stores/validation-errors.store.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type { CategoryError } from '@/modules/categories/interfaces/category.interface';
+
+export const useValidationErrorsStore = defineStore('validation-errors', () => {
+  const errors = ref<string[]>([]);
+
+  interface ValidationError {
+    status: number;
+    message: string;
+    source: string;
+  }
+
+  function getErrors(response: any) {
+    errors.value = [];
+    response.data.errors.forEach((error: ValidationError) => {
+      errors.value[error.source] = error.message;
+    });
+    return errors;
+  }
+
+  return {
+    errors,
+    getErrors,
+  };
+});
