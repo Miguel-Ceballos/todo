bd77e01187d95af2473b18323f44b90084762e90
Miguel Ceballos <mceballosleonides@gmail.com>
2024-09-09 10:45:33 -0600

    Agrega iconos a Login y Register

--- src/modules/common/layouts/Sidebar.vue
+++ src/modules/common/layouts/Sidebar.vue
@@ -7,6 +7,8 @@ import CheckCircleIcon from '@/modules/common/icons/CheckCircleIcon.vue';
 import HashtagIcon from '@/modules/common/icons/HashtagIcon.vue';
 import { useCategoriesStore } from '@/modules/categories/stores/categories.store';
 import { useModalStore } from '@/modules/common/stores/modal.store'
+import UserIcon from '@/modules/common/icons/UserIcon.vue';
+import UserPlusIcon from '@/modules/common/icons/UserPlusIcon.vue';
 
 const categories = useCategoriesStore();
 const modalStore = useModalStore();
@@ -67,11 +69,13 @@ const modalStore = useModalStore();
     <ul class="menu fixed bottom-0 min-w-72">
       <li class="hover:bg-zincs-800 hover:rounded-md">
         <a class="text-gray-400 hover:text-gray-100">
+          <user-icon />
           Log In
         </a>
       </li>
       <li class="hover:bg-zincs-800 hover:rounded-md">
         <a class="text-gray-400 hover:text-gray-100">
+          <user-plus-icon />
           Register
         </a>
       </li>
