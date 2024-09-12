21f2bbb9a085afd4bbd8c6a7e8a25c87431a3e0b
Miguel Ceballos <mceballosleonides@gmail.com>
2024-09-12 15:53:34 -0600

    Agrega función para recuperar el estatus del usuario, valida el login y recupera el token del usuario

--- src/modules/auth/stores/auth.store.ts
+++ src/modules/auth/stores/auth.store.ts
@@ -2,15 +2,14 @@ import { defineStore } from 'pinia';
 import { computed, reactive, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { AuthStatus } from '@/modules/auth/interfaces/auth-status.enum';
-import { loginAction } from '@/modules/auth/actions';
-import { useLocalStorage } from '@vueuse/core';
+import { checkAuthAction, loginAction } from '@/modules/auth/actions';
 
 export const useAuthStore = defineStore('auth', () => {
   const router = useRouter();
 
   const authStatus = ref(AuthStatus.Checking);
   const authenticated = ref(Boolean);
-  const token = ref(useLocalStorage('token', ''));
+  const token = ref('');
 
   const LoginForm = reactive(<LoginParameters>{ email: '', password: '' });
   const RegisterForm = reactive(<RegisterParameters>{});
@@ -33,9 +32,9 @@ export const useAuthStore = defineStore('auth', () => {
         logout();
         return false;
       }
-      token.value = loginResponse.token;
+      localStorage.setItem('token', loginResponse.token);
+      token.value = localStorage.getItem('token');
       authStatus.value = AuthStatus.Authenticated;
-      await router.push({ name: 'home' });
       return true;
     } catch (error) {
       console.log(error);
@@ -44,14 +43,26 @@ export const useAuthStore = defineStore('auth', () => {
   };
 
   const logout = () => {
+    localStorage.removeItem('token');
     authStatus.value = AuthStatus.NotAuthenticated;
     token.value = '';
     return false;
   };
 
-  // const getData = async () => {
-  //   await
-  // }
+  const checkAuthStatus = async (): Promise<boolean> => {
+    try {
+      const statusResp = await checkAuthAction();
+      if (!statusResp.ok) {
+        logout();
+        return false;
+      }
+      authStatus.value = AuthStatus.Authenticated;
+      token.value = statusResp.token;
+    } catch (e) {
+      logout();
+      return false;
+    }
+  };
 
   return {
     LoginForm,
@@ -65,5 +76,6 @@ export const useAuthStore = defineStore('auth', () => {
 
     //Actions
     login,
+    checkAuthStatus,
   };
 });
